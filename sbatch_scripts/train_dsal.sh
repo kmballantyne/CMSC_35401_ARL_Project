285d2a5588ff8aa4ef92a73cd142d9b7d0e5e686
Keegan Mackenzie Ballantyne <kballantyne@fe01.ds.uchicago.edu>
2025-05-26 13:29:05 -0500

    changes made to Slurm script file

--- sbatch_scripts/train_dsal.sh
+++ sbatch_scripts/train_dsal.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=dsal_training
+#SBATCH --job-name=dsal_test
 #SBATCH --output=/home/kballantyne/CMSC_35401_ARL_Project/logs/%j.%N.stdout
 #SBATCH --error=/home/kballantyne/CMSC_35401_ARL_Project/logs/%j.%N.stderr
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=kballantyne@cs.uchicago.edu
 #SBATCH --chdir=/home/kballantyne/CMSC_35401_ARL_Project
 #SBATCH --partition=general
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:1
 #SBATCH --mem=128G
 #SBATCH --time=12:00:00
 
 # Load shell environment
 source /home/${USER}/.bashrc
+
 # Activate your conda environment
 source activate ARL_Project
 
 # Run your training script
-python3 main.py
+python3 main.py --test
